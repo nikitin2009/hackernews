01faa942c7bfe28c7bf744bd30592a96f6178a76
Alex N <nikitin2009@gmail.com>
2019-07-05 16:11:48 +0300

    Updates

---
+++ precache-manifest.68339fb114798f1fb42bce286dbb3f57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8180e92ef15eef466f14b00f2e67043c",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "55343fa7f372c14c0d7e",
+    "url": "/hackernews/static/css/main.075fcfda.chunk.css"
+  },
+  {
+    "revision": "71eb8ecc6217adadefbf",
+    "url": "/hackernews/static/js/2.4ebc86af.chunk.js"
+  },
+  {
+    "revision": "55343fa7f372c14c0d7e",
+    "url": "/hackernews/static/js/main.d5c320d3.chunk.js"
+  },
+  {
+    "revision": "c3e44c618c8e72ba32da",
+    "url": "/hackernews/static/js/runtime~main.a6230328.js"
+  }
+]);
